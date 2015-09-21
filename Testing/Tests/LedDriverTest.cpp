4278e3620e60eaa663ac3af90055e40832cc88e3
mjcardilino <mjcardilino@gmail.com>
2015-09-21 02:36:31 +0000

    Linking test

--- Testing/Tests/LedDriverTest.cpp
+++ Testing/Tests/LedDriverTest.cpp
@@ -1,5 +1,10 @@
 #include "CppUTest/TestHarness.h"
 
+extern "C"
+{
+#include "Source/LedDriver.h"
+}
+
 TEST_GROUP(LedDriver) 
 {
     void setup() 
---
+++ objs/Testing/Tests/LedDriverTest.d
@@ -0,0 +1,43 @@
+objs/Testing/Tests/LedDriverTest.o: Testing/Tests/LedDriverTest.cpp \
+ /home/ubuntu/workspace/Testing/cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h \
+ /home/ubuntu/workspace/Testing/cpputest/include/CppUTest/CppUTestConfig.h \
+ /home/ubuntu/workspace/Testing/cpputest/include/CppUTest/StandardCLibrary.h \
+ /home/ubuntu/workspace/Testing/cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h \
+ /home/ubuntu/workspace/Testing/cpputest/include/CppUTest/TestHarness.h \
+ /home/ubuntu/workspace/Testing/cpputest/include/CppUTest/Utest.h \
+ /home/ubuntu/workspace/Testing/cpputest/include/CppUTest/SimpleString.h \
+ /home/ubuntu/workspace/Testing/cpputest/include/CppUTest/UtestMacros.h \
+ /home/ubuntu/workspace/Testing/cpputest/include/CppUTest/TestResult.h \
+ /home/ubuntu/workspace/Testing/cpputest/include/CppUTest/TestFailure.h \
+ /home/ubuntu/workspace/Testing/cpputest/include/CppUTest/TestPlugin.h \
+ /home/ubuntu/workspace/Testing/cpputest/include/CppUTest/MemoryLeakWarningPlugin.h \
+ /home/ubuntu/workspace/Testing/cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h \
+ Source/LedDriver.h
+
+/home/ubuntu/workspace/Testing/cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h:
+
+/home/ubuntu/workspace/Testing/cpputest/include/CppUTest/CppUTestConfig.h:
+
+/home/ubuntu/workspace/Testing/cpputest/include/CppUTest/StandardCLibrary.h:
+
+/home/ubuntu/workspace/Testing/cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h:
+
+/home/ubuntu/workspace/Testing/cpputest/include/CppUTest/TestHarness.h:
+
+/home/ubuntu/workspace/Testing/cpputest/include/CppUTest/Utest.h:
+
+/home/ubuntu/workspace/Testing/cpputest/include/CppUTest/SimpleString.h:
+
+/home/ubuntu/workspace/Testing/cpputest/include/CppUTest/UtestMacros.h:
+
+/home/ubuntu/workspace/Testing/cpputest/include/CppUTest/TestResult.h:
+
+/home/ubuntu/workspace/Testing/cpputest/include/CppUTest/TestFailure.h:
+
+/home/ubuntu/workspace/Testing/cpputest/include/CppUTest/TestPlugin.h:
+
+/home/ubuntu/workspace/Testing/cpputest/include/CppUTest/MemoryLeakWarningPlugin.h:
+
+/home/ubuntu/workspace/Testing/cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h:
+
+Source/LedDriver.h:
