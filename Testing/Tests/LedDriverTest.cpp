c6de086aa929907b7d78edefdab01ee685b610b1
mjcardilino <mjcardilino@gmail.com>
2015-11-12 04:22:45 +0000

    renamed tests in the form of requirements

--- Testing/Tests/LedDriverTest.cpp
+++ Testing/Tests/LedDriverTest.cpp
@@ -30,101 +30,87 @@ TEST_GROUP(LedDriver)
    }
 };
 
-// Test that LEDs are initialized correctly
-TEST(LedDriver, LedsOffAfterCreate) 
+TEST(LedDriver, LedsShouldBeOffAfterCreate) 
 {
    virtualLeds = 0xffff;
    LedDriver_Create(&virtualLeds);
    LONGS_EQUAL(0, virtualLeds);
 }
 
-// Test that LED 01 is turned on
-TEST(LedDriver, TurnOnLedOne)
+TEST(LedDriver, Led01ShouldTurnOn)
 {
    LedDriver_TurnOn(1);
    LONGS_EQUAL(1, virtualLeds);
 }
 
-// Test that LED 01 is turned off
-TEST(LedDriver, TurnOffLedOne)
+TEST(LedDriver, Led01ShouldTurnOff)
 {
    LedDriver_TurnOn(1);
    LedDriver_TurnOff(1);
    LONGS_EQUAL(0, virtualLeds);
 }
 
-// Test to turn on multiple LEDS
-TEST(LedDriver, TurnOnMultLeds) 
+TEST(LedDriver, MultLedsShouldTurnOn) 
 {
    LedDriver_TurnOn(9);
    LedDriver_TurnOn(8);
    LONGS_EQUAL(0x180, virtualLeds);
 }
 
-// Test to turn on all LEDs
-TEST(LedDriver, AllOn) 
+TEST(LedDriver, AllLedsShouldTurnOn) 
 {
    LedDriver_TurnAllOn();
    LONGS_EQUAL(0xffff, virtualLeds);
 }
 
-// Test to turn off specific LEDs
-TEST(LedDriver, TurnOffAnyLed)
+TEST(LedDriver, ShouldTurnOffAnyLed)
 {
    LedDriver_TurnAllOn();
    LedDriver_TurnOff(8);
    LONGS_EQUAL(0xff7f, virtualLeds);
 }
 
-// Test that shows that the driver is not getting
-// the current LED state from the hardware
-TEST(LedDriver, LedMemoryIsNotReadable) 
+TEST(LedDriver, LedMemoryShoudlNotBeReadable) 
 {
    virtualLeds = 0xffff;
    LedDriver_TurnOn(8);
    LONGS_EQUAL(0x80, virtualLeds);
 }
 
-// Test boundary conditions
-TEST(LedDriver, UpperAndLowerBounds) 
+TEST(LedDriver, UpperAndLowerBoundsShouldTurnOn) 
 {
    LedDriver_TurnOn(1);
    LedDriver_TurnOn(16);
    LONGS_EQUAL(0x8001, virtualLeds);
 }
 
-// Test to handle out-of-bounds (for TurnOn situation)
-TEST(LedDriver, OutOfBoundsTurnOn)
+TEST(LedDriver, TurnOnOutOfBoundsShouldFail)
 {
    CHECK_ASSERTION_FAILED(LedDriver_TurnOn(-1));
    CHECK_ASSERTION_FAILED(LedDriver_TurnOn(300));
 }
 
-// Test to handle out-of-bounds (for TurnOff situation)
-TEST(LedDriver, OutOfBoundsTurnOff)
+TEST(LedDriver, TurnOffOutOfBoundsShouldFail)
 {
    CHECK_ASSERTION_FAILED(LedDriver_TurnOff(-1));
    CHECK_ASSERTION_FAILED(LedDriver_TurnOff(300));
 }
 
-// Test to check if LED is on
-TEST(LedDriver, IsOn)
+TEST(LedDriver, LedShouldBeOn)
 {
    CHECK_EQUAL(false, LedDriver_IsOn(11));
    LedDriver_TurnOn(11);
    CHECK_EQUAL(true, LedDriver_IsOn(11));
 }
 
-// Test to check if LED is off
-TEST(LedDriver, IsOff)
+TEST(LedDriver, LedShouldBeOff)
 {
    CHECK_EQUAL(true, LedDriver_IsOff(12));
    LedDriver_TurnOn(12);
    CHECK_EQUAL(false, LedDriver_IsOff(12));
 }
 
-// Test to turn off multiple LEDs
-TEST(LedDriver, TurnOffMultipleLeds)
+TEST(LedDriver, MultLedsShouldTurnOff)
 {
    LedDriver_TurnAllOn();
    LedDriver_TurnOff(9);
@@ -132,8 +118,7 @@ TEST(LedDriver, TurnOffMultipleLeds)
    LONGS_EQUAL((~0x180)&0xffff, virtualLeds);
 }
 
-// Test to turn off all LEDs
-TEST(LedDriver, AllOff)
+TEST(LedDriver, AllLedsShouldTurnOff)
 {
    LedDriver_TurnAllOn();
    LedDriver_TurnAllOff();
